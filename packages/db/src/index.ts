914c13d52d453cfa993821ab1e35eaa441ecbd22
larskrs <lkssyv@gmail.com>
2026-06-16 01:55:08 +0200

    debug: please

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,7 +3,10 @@ import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema.js';
 
+console.log('Connecting to database...');
+console.log(process.env.DATABASE_URL);
 const client = postgres(process.env.DATABASE_URL ?? 'postgres://starling:starling@localhost:5432/starling');
+console.log(client.name)
 
 export const db = drizzle(client, { schema });
 export * from './schema.js';
